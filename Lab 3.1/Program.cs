29b55c0d74fcda0a25153dab575673ac39323d3b
UserXM230121 <UserXM230121@DESKTOP-591DAC7>
2021-11-09 09:50:46 +0600

    Добавьте файлы проекта.

---
+++ Lab 3.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3.1 средний уровень вариант 15
+            try
+            {
+                Console.WriteLine("Введите число k:");
+                int k = int.Parse(Console.ReadLine());
+                double S = 0;
+                for (int n = 1; n <= k + 1; n++)
+                {
+                    if (n == 1) continue;
+                    S += Math.Pow(-1, 2 * n) * (n * n - 9) * (n * n - 9) / (3 * n)!;
+                }
+                Console.WriteLine($"Y={S:F2}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
